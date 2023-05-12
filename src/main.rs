f70682f9b373d4193a9199366fd425bc0372f2d3
poka <khs0826@dsm.hs.kr>
2023-05-13 02:08:27 +0900

    logger & healthCheck

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,39 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use serde::Serialize;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, Rust!")
+#[derive(Serialize)]
+pub struct GenericResponse {
+    pub status: String,
+    pub message: String,
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+#[get("/get/health")]
+async fn health_checker_handler() -> impl Responder {
+    const MESSAGE: &str = "Build Simple CRUD API with Rust and Actix Web";
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    let response_json = &GenericResponse {
+        status: "success".to_string(),
+        message: MESSAGE.to_string(),
+    };
+    HttpResponse::Ok().json(response_json)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+    env_logger::init();
+
+    println!("🚀 Server started successfully");
+
+    HttpServer::new(move || {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(health_checker_handler)
+            .wrap(Logger::default())
     })
-        .bind(("0.0.0.0", 8080))?
+        .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
